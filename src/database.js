c3d29cc8de02d3dae81dbfc1b2c3df49595c39c7
juanhuamani <jhuamaniv@unsa.edu.pe>
2023-11-29 17:45:25 -0500

    Conexion Base Datos

---
+++ src/database.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+const {database} = require('./keys')
+
+mongoose.connect(database.URI )
+.then(db => console.log('DB is connect '))
+.catch(err => console.log(err))
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const config = require('./server/config')
 
 const app = config(express());
+require('./database');
 
 app.listen(app.get('port'),()=>{
     console.log('Server on port ',app.get('port'))
