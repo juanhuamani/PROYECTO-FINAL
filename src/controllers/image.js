fc8cd29133c926f43e14370d38b974ebc22253fc
juanhuamani <jhuamaniv@unsa.edu.pe>
2023-12-01 19:35:58 -0500

    Guardando datos en DataBaseMongo

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,6 +1,9 @@
 const path = require('path')
-const {randomNumber} = require('../helpers/libs')
 const fs = require('fs-extra')
+
+const {randomNumber} = require('../helpers/libs')
+const {Image} = require('../models/index')
+
 const ctrl = {};
 
 ctrl.index = (req , res)=>{
@@ -8,16 +11,36 @@ ctrl.index = (req , res)=>{
 }
 
 ctrl.create = async (req , res)=>{
-    const imgUrl = randomNumber()
-    const ext = path.extname(req.file.originalname).toLowerCase()
-    const imageTempPath = req.file.path 
-    const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`)
-
-    if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
-        await fs.rename(imageTempPath , targetPath);
+    const  saveImage = async () =>{
+        const imgUrl = randomNumber();
+        const images = await Image.find({filename : imgUrl});
+        if(images.length > 0){
+            saveImage();
+        }else{
+            const ext = path.extname(req.file.originalname).toLowerCase();
+            const imageTempPath = req.file.path ;
+            const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+    
+            if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
+                await fs.rename(imageTempPath , targetPath);
+                const newImg = new Image({
+                    title :  req.body.title ,
+                    filename : imgUrl + ext ,
+                    description : req.body.description ,
+    
+                })
+                const imageSaved = await newImg.save();
+                res.send('works!!!')
+            }
+            else{
+                await fs.unlink(imageTempPath );
+                res.status(500).json({error : 'Only images '})
+            }
+            
+        }
     }
+    saveImage();
 
-    res.send('works!')
 }
 
 ctrl.like = (req , res)=>{
