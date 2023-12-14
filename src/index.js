c386f3b1da6981285703d2428233085a5b0f2708
croko22 <kvchambit@gmail.com>
2023-12-13 22:01:56 -0500

    Change link log port

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const config = require('./server/config')
+const express = require("express");
+const config = require("./server/config");
 
 const app = config(express());
-require('./database');
+require("./database");
 
-app.listen(app.get('port'),()=>{
-    console.log('Server on port ',app.get('port'))
-})
+app.listen(app.get("port"), () => {
+  console.log(`Server on http://localhost:${app.get("port")}`);
+});
