11ef56dc6049ff99ef680e1b698a368e77b22fee
JuanDiego <jhuamaniv@unsa.edu.pe>
2023-12-15 20:49:52 -0500

    Mejorar Controlers/Image/index comment-promesa

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -15,12 +15,18 @@ ctrl.index =async (req , res)=>{
             .then(result => resolve(result))
             .catch(error => reject(error));
     });
-    
+
     if(image){
         image.views += 1 ;
         viewModel.image = image;
         await image.save();
-        const comments = await Comment.find({image_id: image._id});
+        
+        const comments = await new Promise((resolve, reject) => {
+            Comment.find({ image_id: image._id })
+                .then(result => resolve(result))
+                .catch(error => reject(error));
+        });
+
         viewModel.comments = comments;
         viewModel = await sidebar(viewModel)
         res.render('image', viewModel);
