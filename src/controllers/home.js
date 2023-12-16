400afeb0220f12e338599b69b9305f742c30b406
JuanDiego <jhuamaniv@unsa.edu.pe>
2023-12-15 20:41:39 -0500

    Refactor this redundant 'await' on a non-promise

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -4,7 +4,16 @@ const {Image} = require('../models')
 const sidebar = require('../helpers/sidebar')
 
 ctrl.index = async (req , res)=>{
-    const images = await Image.find().sort({timestamp : -1});
+    const images = await new Promise((resolve, reject) => {
+        Image.find().sort({ timestamp: -1 })
+            .then(result => {
+                resolve(result);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+    
     let viewmodel = {images:[]};
     viewmodel.images = images ;
     viewmodel = await sidebar(viewmodel);
