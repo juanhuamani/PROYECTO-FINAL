84af7cf4ce9efd757980974b24a524afa6bf50e1
croko22 <kvchambit@gmail.com>
2023-12-17 21:51:44 -0500

    corregir formato lib.js

--- src/helpers/libs.js
+++ src/helpers/libs.js
@@ -1,15 +1,16 @@
-const helpers = {}
+const helpers = {};
 
-helpers.randomNumber =()=>{
-    const possible = 'abcdefghijklmnopqrstuvwxyz0123456789'
-    let randomNumber = 0 ;
+helpers.randomNumber = () => {
+  const possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let randomNumber = 0;
 
-    for(let i= 0 ; i<6 ; i++){
-        randomNumber += possible.charAt(Math.floor(Math.random() * possible.length))
-    }
+  for (let i = 0; i < 6; i++) {
+    randomNumber += possible.charAt(
+      Math.floor(Math.random() * possible.length)
+    );
+  }
 
-    return randomNumber
+  return randomNumber;
+};
 
-}
-
-module.exports= helpers
+module.exports = helpers;
