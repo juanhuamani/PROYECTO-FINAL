c970817fb9b3dc4bd344fff97216d58933fb070b
JuanDiego <jhuamaniv@unsa.edu.pe>
2023-12-26 12:35:06 -0500

    HomeCoverageSonnar

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -3,12 +3,15 @@ const ctrl = {};
 const {Image} = require('../models')
 const sidebar = require('../helpers/sidebar')
 
-ctrl.index = async (req , res)=>{
-    const images = await Image.find().sort({timestamp : -1});
-    let viewmodel = {images:[]};
-    viewmodel.images = images ;
-    viewmodel = await sidebar(viewmodel);
-    res.render('index', viewmodel )
-}
+ctrl.index = async (req, res) => {
+    try {
+        const images = await Image.find().sort({ timestamp: -1 });
+        let viewmodel = await sidebar({ images });
+        res.render('index', viewmodel);
+    } catch (error) {
+        console.error('Error en el controlador index:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+};
 
 module.exports = ctrl
