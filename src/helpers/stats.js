e650e71353a9e1fc5b625249f794c7fa1e14e5d4
EduardoRojasM <83146468+EduardoRojasM@users.noreply.github.com>
2023-12-27 19:59:25 -0500

    Refactoring Generar Comentarios

--- src/helpers/stats.js
+++ src/helpers/stats.js
@@ -1,59 +1,60 @@
-const { Comment, Image } = require('../models');
+const {Comment , Image} = require('../models') ;
+const images = require('./images');
 
-async function countDocuments(model) {
-    try {
-        return await model.countDocuments();
-    } catch (error) {
-        return 0;
-    }
+async function  imagesCounter(){
+    return await Image.countDocuments();
 }
 
-async function aggregateTotal(model, field) {
+async function commentsCounter(){
+    return await Comment.countDocuments();
+}
+
+async function imagesTotalViewsCounter() {
     try {
-        const result = await model.aggregate([
+        const result = await Image.aggregate([
             {
                 $group: {
                     _id: '1',
-                    [`${field}Total`]: { $sum: `$${field}` }
+                    viewsTotal: { $sum: '$views' }
                 }
             }
         ]);
 
-        return result?.[0]?.[`${field}Total`] || 0;
+        return result?.[0]?.viewsTotal || 0;
+
+    } catch (error) {
+        return 0; 
+    }
+}
+
+async function likeTotalCounter() {
+    try {
+        const result =  Image.aggregate([{
+            $group: {
+                _id: '1',
+                likesTotal: { $sum: '$likes' }
+            }
+        }]);
+
+        return result?.[0]?.likesTotal || 0;
+        
     } catch (error) {
         return 0;
     }
 }
 
-async function imagesCounter() {
-    return await countDocuments(Image);
-}
-
-async function commentsCounter() {
-    return await countDocuments(Comment);
-}
-
-async function imagesTotalViewsCounter() {
-    return await aggregateTotal(Image, 'views');
-}
-
-async function likeTotalCounter() {
-    return await aggregateTotal(Image, 'likes');
-}
-
-module.exports = async () => {
-    const [imagesCount, commentsCount, totalViews, totalLikes] = await Promise.all([
-        imagesCounter(),
-        commentsCounter(),
-        imagesTotalViewsCounter(),
+module.exports = async () =>{
+    const result = await Promise.all([  
+        imagesCounter() ,
+        commentsCounter() ,
+        imagesTotalViewsCounter() ,
         likeTotalCounter()
-    ]);
+    ])
 
     return {
-        images: imagesCount,
-        comments: commentsCount,
-        views: totalViews,
-        likes: totalLikes
-    };
-};
-
+        images : result[0] ,
+        comments : result[1] ,
+        views : result[2] ,
+        likes : result[3]
+    }
+}
